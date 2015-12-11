b68a1ec396583823fb1f81da5ff7a971d8c93a1c
radelmann <robadelmann@gmail.com>
2015-12-10 17:38:04 -0800

    Player content styling

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -3,7 +3,12 @@ var PlayerView = Backbone.View.extend({
 
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
-  el: '<audio controls autoplay />',
+  el: ['<div>',
+      '<img class="song-artwork-url" />',
+      '<div class="song-artist"></div>',
+      '<div class="song-title"></div>',
+      '<audio class="player" controls autoplay />',
+      '</div>'].join(''),
 
   initialize: function() {},
 
@@ -21,6 +26,9 @@ var PlayerView = Backbone.View.extend({
   },
 
   render: function() {
-    return this.$el.attr('src', this.model ? this.model.get('stream_url') + scConfig.qs : '');
+    this.$el.find('.song-artwork-url').attr('src', this.model ? this.model.get('artwork_url') : '');
+    this.$el.find('.song-title').text(this.model ? this.model.get('title') : '');
+    this.$el.find('.song-artist').text(this.model ? this.model.get('user')['username'] : '');
+    return this.$el.find('.player').attr('src', this.model ? this.model.get('stream_url') + scConfig.qs : '');
   }
 });
